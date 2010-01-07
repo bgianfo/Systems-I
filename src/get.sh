f63b9de02d5f78b038c7ee464abc23a27e126d87
Brian Gianforcaro <b.gianfo@gmail.com>
2010-01-06 19:52:10 -0500

    deploy and get

---
+++ src/get.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+HOST='dsl.cs.rit.edu'
+USER='bjg1955'
+PASSWD='bek1AYESHA2sissypu'
+ftp -n -v $HOST << EOT
+ascii
+user $USER $PASSWD
+prompt
+cd sys
+get db.c
+get db.h
+get project1.c
+get project1.h
+get common.h
+get str.h
+get str.c
+put ../header.mak
+EOT
